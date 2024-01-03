44ec5252b73062886c0c238f6ef114a143e20de9
Wolfgang Vinatzer <44369896+GitOnMyLvl@users.noreply.github.com>
2024-01-03 15:00:01 +0100

    implement toggle options for border and rainbow mode

--- javascript/script.js
+++ javascript/script.js
@@ -1,23 +1,30 @@
 const container = document.querySelector('.container');
 const sizeButton = document.querySelector('.size');
 const resetButton = document.querySelector('.reset');
+const borderButton = document.querySelector('.border');
+const rainbowButton = document.querySelector('.rainbow');
 let gridSize = 16;
+let isBorderVisible = false;
+let isRainbowMode = false;
 
 
-function createGrid(gridSize) {
-    for (let i = 0; i < gridSize; i++) {
-        for (let j = 0; j < gridSize; j++) {
+function createGrid(size) {
+    container.innerHTML = '';
+    for (let i = 0; i < size; i++) {
+        for (let j = 0; j < size; j++) {
             const div = document.createElement('div');
-            div.style.width = `${500 / gridSize}px`;
-            div.style.height = `${500 / gridSize}px`;
+            div.style.width = `${500 / size}px`;
+            div.style.height = `${500 / size}px`;
             div.addEventListener('mouseover', () => {
                 if (div.style.backgroundColor === '') {
-                    div.style.backgroundColor = getRandomColor();
+                    div.style.backgroundColor = isRainbowMode ? getRandomColor() : 'black';
                 }
             });
             container.appendChild(div);
+
         }
     }
+    toggleBorder(isBorderVisible);
 }
 
 function getRandomColor() {
@@ -28,20 +35,35 @@ function getRandomColor() {
 }
 
 
+function toggleBorder(isBorderVisible) {
+    let divs = document.querySelectorAll('.container div');
+
+    divs.forEach(div => {
+        div.style.border = isBorderVisible ? '1px solid black' : 'none';
+        div.style.boxSizing = 'border-box';
+    });
+}
+
 
 sizeButton.addEventListener('click', () => {
+    let tempGridSize = gridSize;
     gridSize = prompt('Enter a number between 1 and 100');
-    if (gridSize > 100 || gridSize < 1) {
+    gridSize = parseInt(gridSize);
+    if (!Number.isInteger(gridSize) || gridSize > 100 || gridSize < 1) {
         alert('Resize failed! Enter a number between 1 and 100');
+        gridSize = tempGridSize;
     } else {
-        container.innerHTML = '';
         createGrid(gridSize);
     }
 });
 
-resetButton.addEventListener('click', () => {
-    container.innerHTML = '';
-    createGrid(gridSize);
+resetButton.addEventListener('click', () => createGrid(gridSize));
+
+borderButton.addEventListener('click', () => {
+    isBorderVisible = !isBorderVisible;
+    toggleBorder(isBorderVisible);
 });
+
+rainbowButton.addEventListener('click', () => isRainbowMode = !isRainbowMode);
 
 createGrid(gridSize);
